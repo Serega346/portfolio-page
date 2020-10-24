28ec96f4bed27ade0f34ed4babcead5cd3fa93bd
Serhii Khomyn <serghomyn@gmail.com>
2020-10-24 23:26:41 +0200

    Added About section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -6,14 +6,14 @@ import style from "./style.module.css"
 export const About = () => {
     return(
         <div className="container">
+            <h1 className="mb-5">About me</h1>
             <div className="row">
                 <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12 text-center">
-                    <h1 className="mb-5">About me</h1>
-                    <img src="./images/photoeditorsdk-export.jpeg" alt="It`s me!" className={style.userPhoto}/>
+                    <img src="./images/photoeditorsdk-export.jpeg" alt="It`s me!" className={`${style.userPhoto} img-fluid`}/>
                 </div>
-                <div className="col-xl-9 col-lg-9 col-md-9 col-sm-12 col-12">
-                    <div>
-                        <p></p>
+                <div className="col-xl-9 col-lg-9 col-md-9 col-sm-12 col-12 mt-sm-4 mt-4">
+                    <div className={style.textContainer}>
+                        <p>Hello, my name is Serhii, I am 21 years old and I live in Ivano-Frankivs`k. In 2020, I graduated from Ivano-Frankivs`k National Technical University of Oil and Gas with a bachelor degree in Computer Engineering. Now I am actively looking for a job as a Front-End developer</p>
                     </div>
                 </div>
             </div>
--- src/components/shortInfo/index.jsx
+++ src/components/shortInfo/index.jsx
@@ -10,7 +10,7 @@ export const ShortInfo = () => {
                 <img src="./images/photoeditorsdk-export.jpeg" alt="It`s me!" className={style.userPhoto}/>
                 <h1>Serhii Khomyn</h1>
                 <ReactTypingEffect
-                    text={["Hi i`m developer ", "Hi i`m Front-End developer"]}
+                    text={["Hi it`s my portfolio", "I`m Front-End developer"]}
                     cursorRenderer={cursor => <span>{cursor}</span>}
                     speed={50}
                     eraseSpeed={50}
